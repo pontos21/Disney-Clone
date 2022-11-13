ff7793fcf2c08fc1f18409a55eb67c2f12fa566b
paul serafim <paul.serafim.3021@gmail.com>
2022-11-13 22:30:13 +1100

    login section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 function App() {
@@ -16,7 +16,8 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="detail" element={<Detail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/detail" element={<Detail />} />
           <Route path="/" element={<Home /> } />
         </Routes>
       </Router>
